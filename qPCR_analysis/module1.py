83003ad810a261ae18e86a994de2a8fd00bc1976
noahshanehelton <heltonn@umich.edu>
2024-03-01 14:55:47 -0500

    made file setup and started writing modules and functions

---
+++ qPCR_analysis/module1.py
@@ -0,0 +1,22 @@
+# +
+def import_data(file_path):
+    return True
+
+def average_Ct_technical(gene, condition):
+    return True
+
+def primer_efficiency_calc(slope):
+    efficiency = (10^(-1/slope)-1 ) * 100
+    return efficiency 
+
+def delta_Ct(GOI, control):
+    return True
+
+def delta_delta_Ct():
+    return True
+
+def pfaffl():
+    return True
+
+def polysome_analyis():
+    return True
