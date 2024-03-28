12c593f5e1a064cb0051c341f330f093066a04ef
noahshanehelton <heltonn@umich.edu>
2024-03-28 10:15:30 -0400

    Updated code to perform gene expression ratio analysis with the pfaffl method

--- qPCR_analysis/.ipynb_checkpoints/Data_processing-checkpoint.py
+++ qPCR_analysis/.ipynb_checkpoints/Data_processing-checkpoint.py
@@ -62,20 +62,112 @@ def primer_efficiency(df, gene):
     return primer_efficiency_df
 
 def delta_Ct(GOI, control_gene):
+        '''
+    Goal: 
+
+    Input: 
+
+    Output: 
+    '''
     dCT = GOI - control
     return dCT
 
 def delta_delta_Ct(Gene, experimental_condition, control_condition):
+        '''
+    Goal: 
+
+    Input: 
+
+    Output: 
+    '''
     ddCt = delta_Ct(Gene, control) - delta_Ct(Gene, control)
     return True
 
-def pfaffl(GOI, control_gene, experimental_condition, control_condition, E_GOI, E_control):
-    ratio = (E_GOI**(delta_Ct(GOI, experimental_condition, control_condition)))
+def pfaffl_calc(deltaCT1, deltaCT2, efficiency1, efficiency2):
+    '''
+    Goal: 
+
+    Input: 
+
+    Output: 
+    '''
+    efficiency1 = (efficiency1/100) + 1
+    efficiency2 = (efficiency2/100) + 1
+    ratio = (efficiency1**deltaCT1/efficiency2**deltaCT2)
+    return ratio
+
+
+
+
+
+
+myData = import_and_tidy_data('data/test_data.csv')
+
+def pfaffl(GOI, control_gene, experimental_condition, control_condition, E_GOI, E_control, df):
+    '''
+    Goal: 
+
+    Input: 
+
+    Output: 
+
+
     
-    return df
-
-def polysome_analyis(df):
-    return percent_df
-# -
+    '''
 
 
+    #Get control averages 
+    average_GOI = df[(df['Condition'] == control_condition) & (df['Gene'] == control_gene)]['Ct_value'].mean()
+    average_control = df[(df['Condition'] == control_condition) & (df['Gene'] == GOI)]['Ct_value'].mean()
+
+    # Create a new columns 
+    df['DeltaCt'] = None  # Initialize DeltaCt column with None values
+
+    df.loc[df['Gene'] == control_gene, 'DeltaCt'] = df['Ct_value'] - average_control
+    df.loc[df['Gene'] == GOI, 'DeltaCt'] = df['Ct_value'] - average_GOI
+    df = df[['Gene', 'Condition', 'DeltaCt','Replicate']]
+    df_goi = df[df['Gene'] == 'GOI']  # DataFrame containing rows where 'Gene' is 'GOI'
+    df_control = df[df['Gene'] == 'Control']  # DataFrame containing rows where 'Gene' is 'Control'
+    df_goi.reset_index(drop=True, inplace=True)
+    df_control.reset_index(drop=True, inplace=True)
+
+    GER1 = []
+    GER2 = []
+    GER3 = []
+    for i in range(len(df_goi)):
+        if (df_goi.loc[i, 'Replicate'] == df_control.loc[i, 'Replicate']) and (df_goi.loc[i, 'Condition'] == df_control.loc[i, 'Condition']):
+
+            if df_goi.loc[i, 'Replicate'] == 1: 
+                GER1.append(pfaffl_calc(df_goi['DeltaCt'][i], df_control['DeltaCt'][i], E_GOI, E_control)) 
+            if df_goi.loc[i, 'Replicate'] == 2: 
+                GER2.append(pfaffl_calc(df_goi['DeltaCt'][i], df_control['DeltaCt'][i], E_GOI, E_control)) 
+            if df_goi.loc[i, 'Replicate'] == 3: 
+                GER3.append(pfaffl_calc(df_goi['DeltaCt'][i], df_control['DeltaCt'][i], E_GOI, E_control)) 
+    
+    data = [GER1, GER2, GER3]
+    sem_value = stats.sem(data, axis=0)
+    mean_value = np.mean(data, axis=0)
+
+
+
+    pfaffl_df = pd.DataFrame({
+         'Gene': [GOI, GOI],
+         'Condition': [control_condition, experimental_condition],
+         'Gene Expression Ratio 1':GER1 , 
+         'Gene Expression Ratio 2':GER2 , 
+         'Gene Expression Ratio 3':GER3 , 
+         'Average GER':mean_value , 
+         'SEM GER': sem_value
+     }) 
+
+    return pfaffl_df
+
+
+df = pfaffl('GOI', 'Control', 'Treated', 'Untreated', 100, 100, myData)
+df
+
+
+
+
+#def polysome_analyis(df):
+#    return percent_df
